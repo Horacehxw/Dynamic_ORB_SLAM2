9a5acc1d07c805db57a343942dcd4b92f5f10ea5
horacehxw <horacehxw@gmail.com>
2019-04-29 07:52:07 -0700

    add kitti run script

---
+++ run_kitti_stereo.sh
@@ -0,0 +1,18 @@
+# 00-02
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /home/horacehxw/Data/KITTI/data_odometry_gray/dataset/sequences/00 results/KITTI_stero/00_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /home/horacehxw/Data/KITTI/data_odometry_gray/dataset/sequences/01 results/KITTI_stero/01_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI00-02.yaml /home/horacehxw/Data/KITTI/data_odometry_gray/dataset/sequences/02 results/KITTI_stero/02_Dyna.txt ModelsCNN/
+
+# 03
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI03.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/03 results/KITTI_stero/03_Dyna.txt ModelsCNN/
+
+# 04-10
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/04 results/KITTI_stero/04_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/05 results/KITTI_stero/05_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/06 results/KITTI_stero/06_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/07 results/KITTI_stero/07_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/08 results/KITTI_stero/08_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/09 results/KITTI_stero/09_Dyna.txt ModelsCNN/
+./Examples/Stereo/stereo_kitti Vocabulary/ORBvoc.txt Examples/Stereo/KITTI04-12.yaml ~/Data/KITTI/data_odometry_gray/dataset/sequences/10 results/KITTI_stero/10_Dyna.txt ModelsCNN/
+
+
